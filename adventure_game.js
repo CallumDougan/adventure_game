346cdb1e759403f47b41e1f1d7bc22312afa1093
CallumDougan <practicalitymalfunction@gmail.com>
2016-01-27 14:19:26 +0000

    setup basic objects

--- adventure_game.js
+++ adventure_game.js
@@ -1,3 +1,21 @@
-var Hero = function(){
-  
+var Hero = function(name, health, faveFood){
+  this.name = name;
+  this.health = health;
+  this.faveFood = faveFood;
+}
+
+Hero.prototype = {
+
+  talk: function(){
+
+  },
+  eat: function(){
+
+  }
+}
+
+var Food = function(name, poisoned, repValue){
+  this.name = name;
+  this.poisoned = poisoned;
+  this.repValue = repValue;
 }
