6c82db7b67b8cd49af184aaa2ac9ca0b0f2d6988
CallumDougan <practicalitymalfunction@gmail.com>
2016-01-27 14:30:54 +0000

    setup initial test

--- adventure_game.js
+++ adventure_game.js
@@ -14,8 +14,12 @@ Hero.prototype = {
   }
 }
 
+var hero1 = new Hero('Callum', 100, 'beer');
+
 var Food = function(name, poisoned, repValue){
   this.name = name;
   this.poisoned = poisoned;
   this.repValue = repValue;
 }
+
+module.exports = [hero1]
--- adventure_game_testing.js
+++ adventure_game_testing.js
@@ -1,1 +1,11 @@
-test goes here
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+
+var hero = require('./adventure_game')[0]
+
+describe('hero', function(){
+  it('can speak!!!', function(){
+    expect(hero.talk()).equal("I am a hero called Callum");
+  })
+})
